d14b837a18681df79ef700b890805f23baa4c633
mpscouto <mpscouto@gmail.com>
2020-09-17 17:39:17 -0300

    Inclusão no repositório

---
+++ GeradorMegaSena/frmGerador.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace GeradorMegaSena
+{
+    partial class frmGerador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(107, 258);
+            this.btnGerar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(147, 28);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar Números";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(260, 258);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(147, 28);
+            this.btnFechar.TabIndex = 1;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.Location = new System.Drawing.Point(107, 110);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(40, 37);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "L1";
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2
+            // 
+            this.lbl2.Location = new System.Drawing.Point(155, 110);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(40, 37);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "L2";
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl4
+            // 
+            this.lbl4.Location = new System.Drawing.Point(251, 110);
+            this.lbl4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(40, 37);
+            this.lbl4.TabIndex = 5;
+            this.lbl4.Text = "L4";
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.Location = new System.Drawing.Point(203, 110);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(40, 37);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "L3";
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl6
+            // 
+            this.lbl6.Location = new System.Drawing.Point(347, 110);
+            this.lbl6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(40, 37);
+            this.lbl6.TabIndex = 7;
+            this.lbl6.Text = "L6";
+            this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl5
+            // 
+            this.lbl5.Location = new System.Drawing.Point(299, 110);
+            this.lbl5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(40, 37);
+            this.lbl5.TabIndex = 6;
+            this.lbl5.Text = "L5";
+            this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmGerador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 311);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnGerar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmGerador";
+            this.Text = "Gerador Mega Sena";
+            this.Load += new System.EventHandler(this.frmGerador_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl5;
+    }
+}
---
+++ GeradorMegaSena/frmGerador.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorMegaSena
+{
+    public partial class frmGerador : Form
+    {
+
+        List<int> f_lstNumeros = new List<int>();
+
+        public frmGerador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            f_lstNumeros.Clear();
+            f_lstNumeros.AddRange(SorteiaNumerosSemRepetir(6, 1, 60));
+            Carrega_Numeros();
+
+        }
+
+        private static int[] SorteiaNumerosSemRepetir(int w_intQuantidade, int w_intMinimo, int w_intMaximo)
+        {
+            // Validações dos argumentos.
+            if (w_intQuantidade < 0)
+                throw new ArgumentException("Quantidade deve ser maior que zero.");
+            else if (w_intQuantidade > w_intMaximo + 1 - w_intMinimo)
+                throw new ArgumentException("Quantidade deve ser menor do que a diferença entre máximo e mínimo.");
+            else if (w_intMaximo <= w_intMinimo)
+                throw new ArgumentException("Máximo deve ser maior do que mínimo.");
+
+            List<int> w_lstNumerosSorteados = new List<int>();
+
+            while (w_lstNumerosSorteados.Count < w_intQuantidade)
+            {
+                Random w_rndGeraNumero = new Random();
+                int w_intNumeroSorteado = w_rndGeraNumero.Next(w_intMinimo, w_intMaximo);
+
+                // Número já foi sorteado? Então sorteamos novamente até o número não ter sido sorteado ainda.
+                while (w_lstNumerosSorteados.Contains(w_intNumeroSorteado))
+                {
+                    int w_intVerificaNumero = w_intNumeroSorteado;
+                    w_intNumeroSorteado = w_rndGeraNumero.Next(w_intMinimo, w_intMaximo);
+
+                    if(w_intVerificaNumero == w_intNumeroSorteado + 1 || w_intVerificaNumero == w_intNumeroSorteado - 1) // Evitar que gere números sequenciais
+                    {
+                        w_intNumeroSorteado = w_rndGeraNumero.Next(w_intMinimo, w_intMaximo);
+                    }
+                }
+                    
+                w_lstNumerosSorteados.Add(w_intNumeroSorteado);
+            }
+
+            w_lstNumerosSorteados.Sort(); // Ordena a lista
+            return w_lstNumerosSorteados.ToArray();
+        }
+
+        private void Carrega_Numeros()
+        {
+            try
+            {
+                int w_intCont = 1;
+                List<Label> lbls = new List<Label>()
+                {
+                    lbl1,
+                    lbl2,
+                    lbl3,
+                    lbl4,
+                    lbl5,
+                    lbl6
+                };
+
+                foreach (var lbl in lbls)
+                {
+                    lbl.Text = f_lstNumeros[w_intCont -1].ToString();
+                    w_intCont++;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void frmGerador_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                List<Label> lbls = new List<Label>()
+                {
+                    lbl1,
+                    lbl2,
+                    lbl3,
+                    lbl4,
+                    lbl5,
+                    lbl6
+                };
+
+                int w_intCont = 1;
+                foreach (var lbl in lbls)
+                {
+
+                    lbl.Text = "";
+                    w_intCont++;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
